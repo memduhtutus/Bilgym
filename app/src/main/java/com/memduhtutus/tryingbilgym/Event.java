8bb4f96381acde67dc85743bdf2e7b5d1f1bae45
kerimeren <kerimeren17@gmail.com>
2022-05-05 22:47:51 +0300

    major implementations by Kerim

--- app/src/main/java/com/memduhtutus/tryingbilgym/Event.java
+++ app/src/main/java/com/memduhtutus/tryingbilgym/Event.java
@@ -5,12 +5,18 @@ public class Event {
     String name;
     String SportType;
     matchmakingHour hour;
+    int quota;
 
-    public Event(int alreadyJoined, String name, String SportType, matchmakingHour matchMakingHour) {
+    public Event(int alreadyJoined, String name, String SportType, matchmakingHour matchMakingHour, int quota) {
         this.alreadyJoined = alreadyJoined;
         this.name = name;
         this.SportType = SportType;
         this.hour = matchMakingHour;
+        this.quota = quota;
+    }
+
+    public int getQuota(){
+        return this.quota;
     }
 
 
