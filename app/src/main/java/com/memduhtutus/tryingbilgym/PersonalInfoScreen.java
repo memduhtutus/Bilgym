7a87f8206d394972eb9b11b795835b8163c5049f
kerimeren <kerimeren17@gmail.com>
2022-05-06 15:07:58 +0300

    some methods added for event processes

--- app/src/main/java/com/memduhtutus/tryingbilgym/PersonalInfoScreen.java
+++ app/src/main/java/com/memduhtutus/tryingbilgym/PersonalInfoScreen.java
@@ -32,6 +32,7 @@ public class PersonalInfoScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info_screen);
         binding = ActivityPersonalInfoScreenBinding.inflate(getLayoutInflater());
 
         editText1 = findViewById(R.id.editTextTextPersonName8);
