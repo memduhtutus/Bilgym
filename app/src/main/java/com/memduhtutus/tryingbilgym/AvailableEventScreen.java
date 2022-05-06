4e0205d1cd94a1f2f305937aa82a50ed56c41e98
kerimeren <kerimeren17@gmail.com>
2022-05-06 18:12:16 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/memduhtutus/tryingbilgym/AvailableEventScreen.java
+++ app/src/main/java/com/memduhtutus/tryingbilgym/AvailableEventScreen.java
@@ -1,21 +1,27 @@
 package com.memduhtutus.tryingbilgym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import com.memduhtutus.tryingbilgym.databinding.ActivityAvailableEventScreenBinding;
+import com.memduhtutus.tryingbilgym.databinding.ActivityPersonalInfoScreenBinding;
 
 public class AvailableEventScreen extends AppCompatActivity {
-
+    private @NonNull ActivityAvailableEventScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_event_screen);
+        binding = ActivityAvailableEventScreenBinding.inflate(getLayoutInflater());
     }
 
-    public void AvailableEventsClicked(View view){
+    public void showEventsClicked(View view){
+        String eventText = "";
         for(int i=0; i < MatchmakingScreen.events.size(); i++){
-            MatchmakingScreen.events.get(i).toMap();
+            eventText += MatchmakingScreen.events.get(i).toMap();
+            binding.availableEvents.setText(eventText);
         }
     }
 
