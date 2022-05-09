7100b2d6a1b9136d1a737938f505b84b1761836c
Ahmet Memduh <memduhtutus@gmail.com>
2022-05-09 18:36:09 +0300

    personal trainer methods last

--- app/src/main/java/com/memduhtutus/tryingbilgym/PersonalTrainerScreen.java
+++ app/src/main/java/com/memduhtutus/tryingbilgym/PersonalTrainerScreen.java
@@ -2,7 +2,9 @@ package com.memduhtutus.tryingbilgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalTrainerScreen extends AppCompatActivity {
 
@@ -10,6 +12,26 @@ public class PersonalTrainerScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_trainer_screen);
+    }
+    public void chestClicked (View view){
+        Intent intent = new Intent(PersonalTrainerScreen.this, );
+        startActivity(intent);
+    }
+    public void bicepsClicked (View view){
+        Intent intent = new Intent(PersonalTrainerScreen.this, );
+        startActivity(intent);
+    }
+    public void tricepsClicked (View view){
+        Intent intent = new Intent(PersonalTrainerScreen.this, );
+        startActivity(intent);
+    }
+    public void backClicked (View view) {
+        Intent intent = new Intent(PersonalTrainerScreen.this, );
+        startActivity(intent);
+    }
+    public void shoulderClicked (View view){
+        Intent intent = new Intent(PersonalTrainerScreen.this, );
+        startActivity(intent);
     }
 
 }
