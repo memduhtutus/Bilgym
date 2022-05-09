7e88d1de14e9c68d5b9bfbf2324e9aad9b1d0726
cakpinar1 <cgtykpnr3535@gmail.com>
2022-05-09 18:44:34 +0300

    New classes for muscle types by cagatay

---
+++ app/src/main/java/com/memduhtutus/tryingbilgym/Back.java
@@ -0,0 +1,14 @@
+package com.memduhtutus.tryingbilgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Back extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back);
+    }
+}
---
+++ app/src/main/java/com/memduhtutus/tryingbilgym/Chest.java
@@ -0,0 +1,14 @@
+package com.memduhtutus.tryingbilgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chest);
+    }
+}
