adcc4a67f06debea57f57b088b1bbb78b4b7d5c2
Sarper <ksarper1>
2022-05-09 20:29:50 +0300

    minor adjustments

--- app/src/main/java/com/memduhtutus/tryingbilgym/Shoulder_Screen.java
+++ app/src/main/java/com/memduhtutus/tryingbilgym/Shoulder_Screen.java
@@ -9,6 +9,6 @@ public class Shoulder_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shoulder_screen);
+        setContentView(R.layout.activity_shoulder);
     }
 }
