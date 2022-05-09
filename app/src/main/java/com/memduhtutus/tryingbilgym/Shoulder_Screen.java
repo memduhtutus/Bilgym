c428578e48e1e602401dfbcfeea11f822f65ce4d
Sarper <ksarper1>
2022-05-09 19:49:12 +0300

    personal trainer pages images and texts added

---
+++ app/src/main/java/com/memduhtutus/tryingbilgym/Shoulder_Screen.java
@@ -0,0 +1,14 @@
+package com.memduhtutus.tryingbilgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shoulder_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoulder_screen);
+    }
+}
